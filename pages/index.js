81e59a83966fd09a1e65f4ca7323939f2bfd82a4
pretypographic <a.pesvi@yandex.ru>
2022-11-11 23:48:39 +0400

    сделать сайт

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+let profile = document.querySelector('.profile');
+let editButton = profile.querySelector('.profile__edit-button');
+let profileName = profile.querySelector('.profile__name');
+let profileMission = profile.querySelector('.profile__mission');
+
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-button');
+let popupForm = popup.querySelector('.popup__form');
+let nameInput = popupForm.querySelector('.popup__name');
+let missionInput = popupForm.querySelector('.popup__mission');
+
+let elements = document.querySelector('.elements');
+let reactionButton = elements.querySelector('.element__reaction-button');
+
+function editOpened() {
+    popup.classList.add('popup_opened');
+};
+
+function editClosed() {
+    popup.classList.remove('popup_opened');
+};
+
+function formSubmit() {
+    event.preventDefault();
+    nameInput.value;
+    profileName.textContent = nameInput.value;
+    missionInput.value;
+    profileMission.textContent = missionInput.value;
+    editClosed();
+};
+
+function reaction() {
+    reactionButton.classList.toggle('element__reaction-button_active');
+};
+
+let saveButton = popup.querySelector('.popup__save-button');
+
+editButton.addEventListener('click', editOpened);
+closeButton.addEventListener('click', editClosed);
+popupForm.addEventListener('submit', formSubmit);
+reactionButton.addEventListener('click', reaction);
