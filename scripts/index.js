adc7daf9b9980611c0674e65163e8414dde3480b
pretypographic <a.pesvi@yandex.ru>
2022-11-28 00:23:14 +0400

    исправить

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,63 @@
-let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__edit-button');
-let profileName = profile.querySelector('.profile__name');
-let profileMission = profile.querySelector('.profile__mission');
-let addButton = profile.querySelector('.profile__add-button');
+const profile = document.querySelector('.profile');
+const profileName = profile.querySelector('.profile__name');
+const profileMission = profile.querySelector('.profile__mission');
 
-let popup = document.querySelector('.popup');
-let profileSettings = document.querySelector('#profileSettings');
-let profileForm = profileSettings.querySelector('#profileForm');
-let saveButton = profileSettings.querySelector('.popup__save-button');
-let newLocation = document.querySelector('#newLocation');
-let locationForm = newLocation.querySelector('#locationForm');
+const profileSettings = document.querySelector('#profileSettings');
+const profileForm = profileSettings.querySelector('#profileForm');
+const profileFormName = profileForm.querySelector('.popup__name');
+const profileFormMission = profileForm.querySelector('.popup__mission');
 
-let elements = document.querySelector('.elements');
-let elementTemplate = elements.querySelector('#elementTemplate').content;
-let imageTemplate = document.querySelector('#imageTemplate').content;
+const newLocation = document.querySelector('#newLocation');
+const locationForm = newLocation.querySelector('#locationForm');
+const locationFormName = newLocation.querySelector('.popup__name');
+const locationFormMission = newLocation.querySelector('.popup__mission');
+
+const elements = document.querySelector('.elements');
+
+function openPopup(form) {
+  form.classList.remove('popup_condition_closed');
+  form.classList.add('popup_condition_opened');
+};
+
+function closePopup(form) {
+  form.classList.remove('popup_condition_opened');
+  form.classList.add('popup_condition_closed');
+
+  profileFormName.value = profileName.textContent;
+  profileFormMission.value = profileMission.textContent;
+};
+
+function profileFormSubmit(event) {
+  event.preventDefault();
+
+  profileName.textContent = profileFormName.value;
+  profileMission.textContent = profileFormMission.value;
+
+  closePopup(profileSettings);
+};
+
+function locationFormSubmit(event) {
+  event.preventDefault();
+
+  const location = {
+    name: locationFormName.value,
+    link: locationFormMission.value
+  };
+
+  renderGalery(location);
+  closePopup(newLocation);
+  locationForm.reset();
+};
+
+const editButton = profile.querySelector('.profile__edit-button');
+editButton.addEventListener('click', () => openPopup(profileSettings));
+profileSettings.querySelector('.popup__close-button').addEventListener('click', () => closePopup(profileSettings));
+profileForm.addEventListener('submit', profileFormSubmit);
+
+const addButton = profile.querySelector('.profile__add-button');
+addButton.addEventListener('click', () => openPopup(newLocation));
+newLocation.querySelector('.popup__close-button').addEventListener('click', () => closePopup(newLocation));
+locationForm.addEventListener('submit', locationFormSubmit);
 
 const initialCards = [
     {
@@ -43,78 +87,47 @@ const initialCards = [
 ];
 
 const addElement = function(item) {
+  const elementTemplate = elements.querySelector('#elementTemplate').content;
   const element = elementTemplate.cloneNode(true);
+  const reactionButton = element.querySelector('.element__reaction-button');
+  const trashButton = element.querySelector('.element__trash-button');
+  const beholdImage = element.querySelector('.element__img');
+
   element.querySelector('.element__title').textContent = item.name;  
   element.querySelector('.element__img').setAttribute('src', item.link);
 
-  const reactionButton = element.querySelector('.element__reaction-button');
   function reaction() {
     reactionButton.classList.toggle('element__reaction-button_active');
   };
   reactionButton.addEventListener('click', reaction);
 
-  const trashButton = element.querySelector('.element__trash-button');
   function deleteElement() {
-    trashButton.parentElement.remove();
+    trashButton.closest('.element').remove();
   };
   trashButton.addEventListener('click', deleteElement);
 
-  const beholdImage = element.querySelector('.element__img');
   function openImage() {
-    const behold = imageTemplate.cloneNode(true);
-    behold.querySelector('.behold__image').setAttribute('src', beholdImage.getAttribute('src'));    
-    behold.querySelector('.behold__name').textContent = beholdImage.nextElementSibling.children[0].textContent;
+    const beholdGallery = document.querySelector('#beholdGallery');
 
-    const beholdCloseButton = behold.querySelector('.behold__close-button');
+    beholdGallery.querySelector('.behold__image').setAttribute('src', beholdImage.getAttribute('src'));    
+    beholdGallery.querySelector('.behold__name').textContent = beholdImage.nextElementSibling.children[0].textContent;
+
+    const beholdCloseButton = beholdGallery.querySelector('.behold__close-button');
     function stopBeholding() {
-      beholdCloseButton.parentElement.parentElement.remove();
+      closePopup(beholdGallery);
     };
     beholdCloseButton.addEventListener('click', stopBeholding);
-
-    document.body.prepend(behold);
+    
+    openPopup(beholdGallery);
   };
+
   beholdImage.addEventListener('click', openImage);
 
-  elements.prepend(element);
+  return element;
 };
 
-initialCards.forEach(addElement);
+const renderGalery = function(item) {
+  elements.prepend(addElement(item));
+}
 
-function popupOpened(form) {
-    form.classList.remove('popup_condition_closed');
-    form.classList.add('popup_condition_opened');
-};
-
-function popupClosed(form) {
-    form.classList.remove('popup_condition_opened');
-    form.classList.add('popup_condition_closed');
-};
-
-function profileFormSubmit(event) {
-    event.preventDefault();
-
-    profileName.textContent = profileForm.querySelector('.popup__name').value;
-    profileMission.textContent = profileForm.querySelector('.popup__mission').value;
-
-    popupClosed(profileSettings);
-};
-
-function locationFormSubmit(event) {
-  event.preventDefault();
-
-  const location = {
-    name: locationForm.querySelector('.popup__name').value,
-    link: locationForm.querySelector('.popup__mission').value
-  };
-
-  addElement(location);
-  popupClosed(newLocation);
-};
-
-editButton.addEventListener('click', () => popupOpened(profileSettings));
-profileSettings.querySelector('.popup__close-button').addEventListener('click', () => popupClosed(profileSettings));
-profileForm.addEventListener('submit', profileFormSubmit);
-
-addButton.addEventListener('click', () => popupOpened(newLocation));
-newLocation.querySelector('.popup__close-button').addEventListener('click', () => popupClosed(newLocation));
-locationForm.addEventListener('submit', locationFormSubmit);
+initialCards.forEach(renderGalery);
